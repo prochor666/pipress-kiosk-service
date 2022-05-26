0a2d8bf452bc2157385419868e411c290811e489
prochor666 <prochor666@gmail.com>
2022-05-26 10:47:01 +0200

    API call, media add/remove strategy, validation, config broken

--- pipress/api.py
+++ pipress/api.py
@@ -25,18 +25,25 @@ def sync(conf):
 
         if 'data' in data['layout'] and isinstance(data['layout'], dict) and 'lang' in data:
 
-            core.file_save(
-                f"{local_json_dir}/device.json", json.dumps(data))
-
             if 'media' in data['layout']['data'] and isinstance(data['layout']['data']['media'], dict):
 
-                filter_media(
+                data['layout']['data']['media']['files'] = filter_media(
                     f"{web_data_dir}/media", data['layout']['data']['media'])
 
-                # core.refresh_browser()
+            core.file_save(
+                f"{local_json_dir}/device.json", json.dumps(data))
+            # core.refresh_browser()
+
+    else:
+
+        if os.path.isfile(f"{local_json_dir}/device.json"):
+
+            os.remove(f"{local_json_dir}/device.json")
 
 
 def filter_media(web_data_dir, remote):
+
+    new_files = []
 
     for item in os.listdir(f"{web_data_dir}"):
 
@@ -45,22 +52,41 @@ def filter_media(web_data_dir, remote):
 
             local_size = os.path.getsize(f"{web_data_dir}/{item}")
 
-            if file_basename not in remote['files']:
+            #if file_basename not in remote['files']:
+            search = search_file(file_basename, remote['files'])
+
+            if search == -1:
+
                 print(f"Remove {item}, not used anymore")
                 os.remove(f"{web_data_dir}/{item}")
             else:
 
-                if file_basename in remote['files'] and local_size != remote['files'][item]['size']:
+                if search > -1 and local_size != remote['files'][search]['size']:
                     print(
-                        f"Remove {item}, size remote: {remote['files'][item]['size']}/local: {local_size} and download again, size changed")
-                    os.remove(f"{web_data_dir}/{item}")
+                        f"Remove {item}, size remote: {remote['files'][search]['size']}/local: {local_size} and download again, size changed")
 
-                if file_basename in remote['files'] and local_size == remote['files'][item]['size']:
+                    os.remove(f"{web_data_dir}/{item}")
+                    new_files.append(remote['files'][search])
+
+                if search > -1 and local_size == remote['files'][search]['size']:
                     print(
-                        f"No change on {item}, size remote: {remote['files'][item]['size']}/local: {local_size}, same file/size")
-                    remote['files'].pop(item)
+                        f"No change on {item}, size remote: {remote['files'][search]['size']}/local: {local_size}, same file/size")
+                    new_files.append(remote['files'][search])
+                    remote['files'].pop(search)
 
     download_new_media(web_data_dir, remote)
+    return new_files
+
+
+def search_file(file, files):
+    index = 0
+    for item in files:
+
+        if item["basename"] == file:
+            return index
+        index += 1
+
+    return -1
 
 
 def download_new_media(web_data_dir, remote):
